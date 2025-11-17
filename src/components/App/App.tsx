ebf7e7aedca65ba40d2252a15ccd1a0297966f28
nikolka44 <zotikovich44@gmail.com>
2025-11-17 13:47:32 +0200

    added structure and Cafeinfo

---
+++ src/components/App/App.tsx
@@ -0,0 +1,21 @@
+import css from "./App.module.css";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import { useState } from "react";
+import type { Votes } from "../../Types/votes";
+
+function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+  return (
+    <>
+      <div className={css.app}>
+        <CafeInfo />
+      </div>
+    </>
+  );
+}
+
+export default App;
