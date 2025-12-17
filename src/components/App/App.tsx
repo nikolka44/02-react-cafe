7aff5fa4f44a4694734af7ed59a6dff012d15d8e
nikolka44 <zotikovich44@gmail.com>
2025-12-17 11:59:05 +0200

    rename type

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,8 @@
 import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo";
 import { useState } from "react";
-import type { Votes } from "../../types/votes";
-import type { VoteType } from "../../types/votes";
+import type { Votes } from "../../common/votes";
+import type { VoteType } from "../../common/votes";
 import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../VoteStats/VoteStats";
 import Notification from "../Notification/Notification";
